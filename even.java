80ae807c945e41abaeebfc5b826ff0d80b15a30a
poorni1234 <47376824+poorni1234@users.noreply.github.com>
2019-03-08 02:37:02 -0800

    Create even.java

---
+++ even.java
@@ -0,0 +1,23 @@
+import java.Scanner.util.*.;
+public class even
+ {
+  public static void main(String[]args)
+ {
+  Scanner sc=new Scanner(System.in)
+  int n=sc.next?Int();
+  System.out.println("the odd numbers in interval:");
+  for(int 1=0;i<=5;i++)
+{
+  if(i%2==0)
+{
+  i=d;
+  System.out.println(i);
+}
+  else
+{
+  System.out.println("wrong entry"):
+}
+}
+  System.out.println(n);
+}
+}
